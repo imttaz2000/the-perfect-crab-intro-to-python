079f477e1e1eee76632dd1c8b19770021177b75d
Tasnia Begum <81173279+imttaz2000@users.noreply.github.com>
2025-07-27 23:31:26 +0100

    Update 037_dicts.py
    Added dictionary definition to the dictionary and printed it

--- 037_dicts.py
+++ 037_dicts.py
@@ -17,7 +17,9 @@
 my_dictionary = {
   "String": "A sequence of characters",
   "List": "A sequence of any item",
+  "Dictionary": "A collection of keys mapped to values",  # 👈 ADD THIS
 }
+
 
 # Note that:
 #
@@ -37,6 +39,6 @@ print("  " + my_dictionary["List"])
 # the code around line 20. Then print out the value below.
 
 print("A Dictionary is:")
-# ...
+print("  " + my_dictionary["Dictionary"])
 
 # Once you're done, move on to 038_dict_operations.py
